145f888f8e4e481761c1df1b7793deb327712ea4
unknown <daniel.barreleiro@pm.me>
2025-07-27 11:51:15 +0100

    Initial commit

---
+++ functions/api/dealership.js
@@ -0,0 +1,26 @@
+export async function onRequest(context) {
+  try {
+    const response = await fetch('https://tycoon-2epova.users.cfx.re/status/dealership.json', {
+      headers: {
+        'X-Tycoon-Key': context.env.TYCOON_API_KEY
+      }
+    });
+    
+    const data = await response.json();
+    
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed fetching data' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
+}
